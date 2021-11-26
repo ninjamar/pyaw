b4a19fef155f840afb14509cd996ff33b446e725
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-11-26 13:06:15 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as f:
+  long_description = f.read()
+
+setuptools.setup(
+	name='pyaw',
+  	version='0.0.1',
+  	description='Python Assembly Wrapper',
+  	author='ninjamar',
+  	url='https://github.com/ninjamar/pyaw',
+  	packages=['pyaw'],
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	license_files = ("LICENSE",),
+	python_requires='>=3.7',  
+)
