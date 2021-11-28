8ad606ad5c53155a1a1a874b7b3f96a076564035
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-11-28 14:34:35 +0800

    forgot to change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
 	name='pyaw',
-  	version='0.0.1',
+  	version='0.0.2',
   	description='Python Assembly Wrapper',
   	author='ninjamar',
   	url='https://github.com/ninjamar/pyaw',
